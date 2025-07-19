e82b2888878a251192069fb223b8bed041e8fdab
yashween <yashwin.badass@gmail.com>
2025-07-20 01:20:36 +0530

    Create FloatingButton.vue

---
+++ components/FloatingButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="floating-btn" @click="$emit('toggle')">
+    ⚙️
+  </button>
+</template>
+
+<style scoped>
+.floating-btn {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background: #333;
+  color: #fff;
+  border: none;
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  font-size: 24px;
+  cursor: pointer;
+}
+</style>
